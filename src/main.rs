6294f4a118b9239484064502894bc46743d7e95f
wsl <a@neb.best>
2025-03-26 14:40:24 -0400

    maybe the lexer is actually an iterator??

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::{ops::Range, str::Chars};
+
+use peekmore::{PeekMore, PeekMoreIterator};
 struct StrPieces<'a> {
     pieces: &'a [&'a str],
 }
@@ -10,11 +13,68 @@ impl<'a> From<StrPieces<'a>> for String {
         s
     }
 }
+// fn main() {
+//     let string = "Hello something World!".to_string();
+//     let sp = StrPieces {
+//         pieces: &[&string[0..5], &string[5..6], &string[16..21]],
+//     };
+//     let stringified: String = sp.into();
+//     dbg!(&stringified);
+// }
+
+impl<'a> PartialEq for StrPieces<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        let self_iter = self.pieces.iter().flat_map(|s| s.chars());
+        let other_iter = other.pieces.iter().flat_map(|s| s.chars());
+        self_iter.eq(other_iter) // Compare character by character
+    }
+}
+
+pub struct CLexerIterator<'a> {
+    chars: PeekMoreIterator<Chars<'a>>,
+    current_pos: usize,
+}
+#[derive(Debug)]
+pub enum Tok {
+    Char(char),
+    Whitespace,
+    Error,
+}
+impl<'a> Iterator for CLexerIterator<'a> {
+    type Item = (Range<usize>, Tok);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let c = self.chars.next()?;
+
+        let token = if c == '\\' && self.chars.peek_nth(0).map(|c| *c) == Some('\n') {
+            self.next().unwrap();
+            self.current_pos += 1;
+            self.next()
+        } else {
+            Some((
+                self.current_pos..self.current_pos + 1,
+                match c {
+                    ' ' | '\t' | '\n' => Tok::Whitespace,
+                    _ => Tok::Error,
+                },
+            ))
+            // Some((self.current_pos..self.current_pos + 1, Tok::Char(c)))
+        };
+        self.current_pos = self.current_pos + 1;
+        token
+    }
+}
+
 fn main() {
-    let string = "Hello something World!".to_string();
-    let sp = StrPieces {
-        pieces: &[&string[0..5], &string[5..6], &string[16..21]],
+    let source = "int main\\\na";
+    let mut i = CLexerIterator {
+        current_pos: 0,
+        chars: source.chars().peekmore(),
     };
-    let stringified: String = sp.into();
-    dbg!(&stringified);
+    let tokens = i.collect::<Vec<_>>();
+    dbg!(&tokens);
+    // while let Some((span, ch)) = i.next() {
+    //     // println!("{:#?}", token);
+    //     assert_eq!(&source[span], format!("{}", ch).as_str());
+    // }
 }
